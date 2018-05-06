c5b8c3e1e91de8487d3395312d9b278d657eb78f
Dimitrios Stoupis <dimitris.apple@gmail.com>
2018-05-06 10:47:32 +0000

    Improved the class

--- requestHandler.py
+++ requestHandler.py
@@ -1,3 +1,4 @@
+from PyQt5 import QtCore
 import motorDriver
 import logData_Pi
 import threading
@@ -7,21 +8,29 @@ num_of_stp_per_deg_ra = 100  # Enter the number of steps per degree for the RA m
 num_of_stp_per_deg_dec = 430  # Enter the number of steps per degree for the DEC motor (10000 steps/deg)
 
 
-class requestHandle(object):
-    def __init__(self, cfg_data):
+class requestHandle(QtCore.QObject):
+    def __init__(self, cfg_data, server, client, parent=None):
+        super(requestHandle, self).__init__(parent)  # Get the parent of the class
         self.motor = motorDriver.motor()
         self.log_data = logData_Pi.logData(__name__)
         self.cfg_data = cfg_data
+        self.server = server
+        self.client = client
 
-    def process(self, request):
+    def start(self):
+        print("Handler thread started ID: %d", int(QtCore.QThread.currentThreadId()))
+
+    @QtCore.pyqtSlot(str, name='requestProcess')
+    def process(self, request: str):
         response = "None"  # Variable to hold the response to be sent
 
-        if (request == None) or (request == ""):  # Check if the client is disconnected without any notice
+        if request == "":  # Check if the client is disconnected without any notice
             response = "None"
         elif request == "Test":  # Respond to the connection testing command
             response = "OK"
         elif request == "Terminate":  # Send the required response for the successful termination
             self.log_data.log("INFO", "Client requested connection termination.")
+            self.server.releaseClient()
             response = "Bye"
         elif request == "Quit":  # Send the 'Quit' response, which indicates server closing
             response == "Server closing"
@@ -36,7 +45,8 @@ class requestHandle(object):
             self.log_data.log("INFO", "Received \'%s\' from client" % request)
             compon = request.split("_")  # Get the components of the string
             if compon[0] == "TRNST":
-                '''
+                # TODO review the transit function and correct any mistakes
+                ''' 
                     #self.RAmotor
                     #Handle the transit function
                     #Get the RA and DEC and convert to number, after that calculate the number of steps
@@ -103,6 +113,7 @@ class requestHandle(object):
 
             elif compon[0] == "AAF":
                 # Handle the aim and follow function
+                # TODO implement the tracking function
                 print("Received AAF")
             elif compon[0] == "SCAN":
                 # Handle the scan function
@@ -115,4 +126,4 @@ class requestHandle(object):
                 self.log_data.log("WARNING",
                                   "The command: %s, is not recognized as a valid server command, so no action taken." % request)
                 response = "The command: %s, is not recognized as a valid server command." % request
-        return response
+        self.server.sendDataClient.emit(response)  # Send the response to the client
