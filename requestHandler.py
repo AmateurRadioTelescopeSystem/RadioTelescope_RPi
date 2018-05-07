7e3b16b71c230ec8eb7028468046e6c6ef460b54
Dimitrios Stoupis <dimitris.apple@gmail.com>
2018-05-07 19:38:30 +0000

    Added some more requestes for testing purposes

--- requestHandler.py
+++ requestHandler.py
@@ -1,5 +1,4 @@
 from PyQt5 import QtCore
-import motorDriver
 import logData_Pi
 import sys
 
@@ -9,9 +8,8 @@ num_of_stp_per_deg_dec = 430  # Enter the number of steps per degree for the DEC
 
 
 class requestHandle(QtCore.QObject):
-    def __init__(self, cfg_data, server, client, servThread, clientThread, positionThread, parent=None):
+    def __init__(self, cfg_data, server, client, posObj, servThread, clientThread, positionThread, parent=None):
         super(requestHandle, self).__init__(parent)  # Get the parent of the class
-        self.motor = motorDriver.motor()
         self.log_data = logData_Pi.logData(__name__)
         self.cfg_data = cfg_data
         self.server = server
@@ -19,6 +17,7 @@ class requestHandle(QtCore.QObject):
         self.positionThread = positionThread
         self.serverThread = servThread
         self.clientThread = clientThread
+        self.posObj = posObj
 
     def start(self):
         print("Handler thread started ID: %d" % int(QtCore.QThread.currentThreadId()))
@@ -40,9 +39,12 @@ class requestHandle(QtCore.QObject):
         elif request == "STOP_POS_SEND":
             self.positionThread.quit()
             response = "POSITION_REPORTING_HALTED"
+        elif request == "SEND_POS_UPDATE":
+            cur_pos = self.posObj.getPosition()
+            self.client.sendData.emit("POSUPDATE_RA_%.5f_DEC_%.5f\n" % (float(cur_pos[0]), float(cur_pos[1])))
+            response = "POS_UPDT_SENT"
         elif request == "STOP":  # TODO implement the stop request in a better way
-            response = "We are stopping"
-            sys.exit()
+            sys.exit()  # Exit from the application as per request
         elif request == "Test":  # Respond to the connection testing command
             response = "OK"
         elif request == "Terminate":  # Send the required response for the successful termination
@@ -50,7 +52,7 @@ class requestHandle(QtCore.QObject):
             self.server.releaseClient()
             response = "Bye"
         elif request == "Quit":  # Send the 'Quit' response, which indicates server closing
-            response == "Server closing"
+            response = "Server closing"
         elif request == "Report Position":  # Respond with the current position
             # Add code to calculate and send the current position of the telescope to the client as requested
             response = "POS_0_40_12_34"  # RA_DEC_STPRA_STPDEC
