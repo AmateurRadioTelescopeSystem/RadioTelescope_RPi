be5b5c16a880ece22629522a01d0a4957784ed78
Dimitrios Stoupis <dimitris.apple@gmail.com>
2019-03-21 20:38:37 +0000

    Add a missing package fetching

--- Core/RadioT_RPi.py
+++ Core/RadioT_RPi.py
@@ -16,43 +16,42 @@ from Core.Configuration import configData_Pi, defaultData
 
 # pylint: disable=wrong-import-position
 
-# TODO Test the functionality of the server and client
-# TODO make the app "exitable" so killing is not required for termination
-
-# Create the directory for the log files if it does not exist already
-try:
-    if not os.path.exists('logs'):
-        os.makedirs('logs')
-except Exception as excep:
-    print("There is a problem with the log directory. See tracback: \n%s" % excep, file=sys.stderr)
-    sys.exit(-1)  # Exit the program if an error occurred
-
-# Check if the logging configuration file exists
-try:
-    if not os.path.exists('Settings/log_config.ini'):
-        print("Logging configuration file not found. Creating the default.", file=sys.stderr)
-        log_file = open("Settings/log_config.ini", "w+")  # Open the logging configuration file in writing mode
-        log_file.write(defaultData.log_config_str)  # Write the default dat to the file
-        log_file.close()  # Close the file, since no other operation required
-except Exception as excep:
-    print("There is a problem creating the logging configuration file. See tracback: \n%s" % excep, file=sys.stderr)
-    sys.exit(-1)  # Exit the program if an error occurred
-
-# Check if the settings XML file exists
-try:
-    if not os.path.exists('Settings/settings_pi.xml'):
-        print("Settings file not found. Creating the default.", file=sys.stderr)
-        setngs_file = open("Settings/settings_pi.xml", "w+")  # Open the settings file in writing mode
-        setngs_file.write(defaultData.settings_xml_str)  # Write the default dat to the file
-        setngs_file.close()  # Close the file, since no other operation required
-except Exception as excep:
-    print("There is a problem creating the settings file. See tracback: \n%s" % excep, file=sys.stderr)
-    sys.exit(-1)  # Exit the program if an error occurred
-
-log_data = logging.config.fileConfig('Settings/log_config.ini')  # Get the and apply the logger configuration
-
 
 def main():
+    # TODO Test the functionality of the server and client
+    # TODO make the app "exitable" so killing is not required for termination
+
+    # Create the directory for the log files if it does not exist already
+    try:
+        if not os.path.exists('logs'):
+            os.makedirs('logs')
+    except Exception as excep:
+        print("There is a problem with the log directory. See tracback: \n%s" % excep, file=sys.stderr)
+        sys.exit(-1)  # Exit the program if an error occurred
+
+    # Check if the logging configuration file exists
+    try:
+        if not os.path.exists('Settings/log_config.ini'):
+            print("Logging configuration file not found. Creating the default.", file=sys.stderr)
+            log_file = open("Settings/log_config.ini", "w+")  # Open the logging configuration file in writing mode
+            log_file.write(defaultData.log_config_str)  # Write the default dat to the file
+            log_file.close()  # Close the file, since no other operation required
+    except Exception as excep:
+        print("There is a problem creating the logging configuration file. See tracback: \n%s" % excep, file=sys.stderr)
+        sys.exit(-1)  # Exit the program if an error occurred
+
+    # Check if the settings XML file exists
+    try:
+        if not os.path.exists('Settings/settings_pi.xml'):
+            print("Settings file not found. Creating the default.", file=sys.stderr)
+            setngs_file = open("Settings/settings_pi.xml", "w+")  # Open the settings file in writing mode
+            setngs_file.write(defaultData.settings_xml_str)  # Write the default dat to the file
+            setngs_file.close()  # Close the file, since no other operation required
+    except Exception as excep:
+        print("There is a problem creating the settings file. See tracback: \n%s" % excep, file=sys.stderr)
+        sys.exit(-1)  # Exit the program if an error occurred
+
+    logging.config.fileConfig('Settings/log_config.ini')  # Get the and apply the logger configuration
     cfg = configData_Pi.confDataPi("settings_pi.xml")  # Parse the configuration file and create the object
 
     app = QtCore.QCoreApplication(sys.argv)  # Create application object
